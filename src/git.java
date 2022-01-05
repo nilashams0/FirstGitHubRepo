36f838c13829f6e46fd285b7090c2cfc7f4a852d
nilashams0 <97071732+nilashams0@users.noreply.github.com>
2022-01-04 19:52:36 -0500

    Update git.java
    This is for Practice

--- src/git.java
+++ src/git.java
@@ -5,4 +5,9 @@ public class git {
 }
 public void methodA(){
   system.out.println("This is change from upstream means is remote Repo");
-  
+}
+
+public void methodB(){
+  //this is second mrthod for practice purpose
+
+}
