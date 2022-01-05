633c6cff9bb56a5ef174494017dd1f97dd22014e
nilashams0 <masd.shamsmail@gmail.com>
2022-01-04 21:41:13 -0500

    test test

--- src/git.java
+++ src/git.java
@@ -1,19 +1,18 @@
 
 public class git {
 
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		
 	}
-=======
   
-}
+
 public void methodA(){
-  system.out.println("This is change from upstream means is remote Repo");
+  System.out.println("This is change from upstream means is remote Repo");
 }
 
 public void methodB(){
-  //this is second mrthod for practice purpose only
+  //this is second method for practice purpose only
 
->>>>>>> ae917255310e4d1fa5f7244cbdd531ce621072fa
 }
+}
+
